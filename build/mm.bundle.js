df07be9765d096ed82978d6be496021842fb8872
auxting <30806723+auxting@users.noreply.github.com>
2017-08-10 12:29:33 -0600

    Add files via upload

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -11256,7 +11256,7 @@ angular.module('mm.core.login', [])
     .state('mm_login.site', {
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
-        controller: 'mmLoginSiteCtrl'
+        controller: 'mmLoginSiteCtrl',
           onEnter: function($state) {
         $state.go('mm_login.credentials', {siteurl: 'http://104.198.184.204'});
     }
